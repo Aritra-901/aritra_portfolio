f6fd17797c35fa22667e8d86e926e422eb3afc52
Aritra-901 <78530215+Aritra-901@users.noreply.github.com>
2026-01-18 14:51:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
